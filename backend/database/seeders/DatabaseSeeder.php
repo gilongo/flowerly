d7098de5b56052a541726161de491f9440547d18
gilongo <brasco142.cod@outlook.it>
2024-07-19 09:54:37 +0200

    added Product factory for seeding the database + added seeding at application start

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Product;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Flowerly User',
+            'email' => 'test@flowerly.com',
+            'password' => Hash::make('password'),
         ]);
+
+
+        Product::factory(10)->create();
     }
 }
