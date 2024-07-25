55f399777be16e1d3a925126feb775029688f9e1
giolongo <longo@polarisengineeringspa.com>
2024-07-25 21:15:40 +0200

    started designing create order page

--- frontend/src/app/create-order/create-order.component.ts
+++ frontend/src/app/create-order/create-order.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-order',
   standalone: true,
-  imports: [RouterLink, MatButtonModule],
+  imports: [RouterLink, MatButtonModule, MatFormFieldModule, MatInputModule, FormsModule],
   templateUrl: './create-order.component.html',
   styleUrl: './create-order.component.css'
 })
 export class CreateOrderComponent {
 
+  customerId = '';
+
+  description = '';
 }
