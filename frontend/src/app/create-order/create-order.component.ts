4da57e08fec082bb7925ceeac583ee31db2b9b24
giolongo <longo@polarisengineeringspa.com>
2024-07-25 15:55:39 +0200

    defined style in create order

--- frontend/src/app/create-order/create-order.component.ts
+++ frontend/src/app/create-order/create-order.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-create-order',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule],
   templateUrl: './create-order.component.html',
   styleUrl: './create-order.component.css'
 })
